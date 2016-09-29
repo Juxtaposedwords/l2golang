70a3532ee67bb32c14501a0f87abe243a2d16d17
Maloy Wilkes <Juxtaposedwords@gmail.com>
2016-09-28 22:31:47 -0500

    sudo add-apt-repository ppa:ubuntu-lxc/lxd-stable

--- 01-gowiki/wiki.go
+++ 01-gowiki/wiki.go
@@ -29,8 +29,23 @@ func viewHandler(w http.ResponseWriter, r *http.Request){
 	p, _ := loadPage(title)
 	fmt.Fprintf(w, "<h1>%s</h1> <div>%s</div>", p.Title, p.Body)
 }
+func editHandler(w http.ResponseWriter, r *http.Request){
+  title :=r.URL.Path[len("/edit/"):]
+  p, err := loadPage(title)
+  if err!= nil {
+    p = &Page{Title: title}
+  }
+  fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+    "<form action=\"/save/%s\" method=\"POST\">"+
+    "<textarea name=\"body\">%s</textarea><br>"+
+    "<input type=\"submit\" value=\"Save\">"+A
+    "</form>",
+    p.Title, p.Title, p.Body)
+}
 func main(){
 	http.HandleFunc("/view/", viewHandler)
+  http.HandleFunc("/edit/", editHandler)
+  http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
