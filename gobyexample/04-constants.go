2516f3e03dca6515a2864f31106a7073766bebfd
juxtaposedwords <samuelmaloy@samuelmaloy-macbookpro.roam.corp.google.com>
2016-10-03 18:32:17 -0700

    adding comments since they're in the guide

--- gobyexample/04-constants.go
+++ gobyexample/04-constants.go
@@ -9,7 +9,10 @@ func main() {
 	fmt.Println(s)  // The given constant can appear anywehre a var statement can
 	
 	const n = 500000000
-
+	
+// const statement can appear anywehre a var statement can
+// const perform arithemtic with arbitrary precisions
+// A numermic constant has no type until it's given one, such as by explicit cast
 	const d = 3e20 / n
 	fmt.Println(d)
 
