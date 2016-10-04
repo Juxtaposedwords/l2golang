5c0f12407badda17480b4a26a9081aa39f3aee47
juxtaposedwords <samuelmaloy@samuelmaloy-macbookpro.roam.corp.google.com>
2016-10-03 18:24:44 -0700

    lerning bout dem variables

---
+++ gobyexample/03-variables.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "intial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b,c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	
+	f := "short" // := is shortahnd for declaring and intiializinga vairable
+							 // same as var f string = "short"
+	fmt.Println(f)
+}
