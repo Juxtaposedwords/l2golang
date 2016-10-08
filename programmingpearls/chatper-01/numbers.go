1058e4339191c3242785159233590cef4008d5e2
Maloy Wilkes <Juxtaposedwords@gmail.com>
2016-10-08 00:03:14 -0500

    check in so i can tinker elsewhere

--- programmingpearls/chatper-01/numbers.go
+++ programmingpearls/chatper-01/numbers.go
@@ -1,1 +1,14 @@
-// Generate K unique integers from 1 - 27,000
+// Generate K unique integers from 1 - 27,0100
+package main
+import "fmt"
+
+// none of this works yo
+func main() {
+  collection := make([]int, 27000)
+  counter    := 0
+  for {
+    collection
+    fmt.Println(counter)
+    counter = counter + 1
+  }
+}
