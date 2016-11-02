5d9ed6ff657441b143ec8e5919a38fe347f4f425
juxtaposedwords <samuelmaloy@samuelmaloy-macbookpro.roam.corp.google.com>
2016-11-01 21:34:19 -0700

    checking in this stuff

---
+++ golang/tour/02-flowcontrol/08-exercise-loop-and-functions.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Sqrt(x float64) float64 {
+	z := float64(x)
+	for i:=0; i < 10; i++ {
+		z = z - (z*z - x)/(2*z)
+	}
+	return z
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+}
