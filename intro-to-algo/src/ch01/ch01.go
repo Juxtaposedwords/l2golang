69b046c59dc8dd2e0c92bd7c49852f1a20e7f618
juxtaposedwords <samuelmaloy@samuelmaloy-macbookpro.roam.corp.google.com>
2016-11-06 20:17:48 -0800

    errr somehow the outter for loop got dropped

--- intro-to-algo/src/ch01/ch01.go
+++ intro-to-algo/src/ch01/ch01.go
@@ -2,13 +2,12 @@ package ch01
 
 
 func insertSort(A []int) {
+	for j := 0; j < len(A); j++ {
 		// store the number we're sorting on in 'key'
 		key := A[j]
 		// store the location in the array
 		// This implicity tells us everything before i is sorted
 		i := j -1
-		fmt.Printf("Item: j: %d\n", j)
-		fmt.Printf("%z\n", A)
 		// From the last sort digit to the first (so Right to Left)
 		//     if this item is greater than key, move that value one
 		//     to the right and decrement the counter
@@ -17,5 +16,6 @@ func insertSort(A []int) {
 			i = i -1
 		} 
 		A[i+1] = key
+	}
 
 }
