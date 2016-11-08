74818bfe45214e504e108fc22553cf5a9b122ea7
juxtaposedwords <samuelmaloy@samuelmaloy-macbookpro.roam.corp.google.com>
2016-11-07 20:57:11 -0800

    per 2.1 did a silly little decrementing loop, rather than incrementing

---
+++ intro-to-algo/src/ch02/ch02_test.go
@@ -0,0 +1,83 @@
+package ch02
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestInsertSort(t *testing.T) {
+	got := []int{5, 2, 4, 6, 1, 3}
+	want := []int{1, 2, 3, 4, 5, 6}
+	insertSort(got)
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("insertSort():  got %v, want %v", got, want)
+	}
+}
+func TestInsertSortDuplicates(t *testing.T) {
+	got := []int{5, 2, 4, 6, 1, 3, 1}
+	want := []int{1, 1, 2, 3, 4, 5, 6}
+	insertSort(got)
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("insertSort():  got %v, want %v", got, want)
+	}
+}
+
+func TestInsertSortLargeValues(t *testing.T) {
+	got := []int{5, 2, 2584, 6, 1, 3}
+	want := []int{1, 2, 3, 5, 6, 2584}
+	insertSort(got)
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("insertSort():  got %v, want %v", got, want)
+	}
+}
+
+func TestReverseInsertSort(t *testing.T) {
+	got := []int{5, 2, 4, 6, 1, 3}
+	want := []int{1, 2, 3, 4, 5, 6}
+	reverseInsertSort(got)
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("insertSort():  got %v, want %v", got, want)
+	}
+}
+func TestReverseInsertSortDuplicates(t *testing.T) {
+	got := []int{5, 2, 4, 6, 1, 3, 1}
+	want := []int{1, 1, 2, 3, 4, 5, 6}
+	reverseInsertSort(got)
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("insertSort():  got %v, want %v", got, want)
+	}
+}
+
+func TestReverseInsertSortLargeValues(t *testing.T) {
+	got := []int{5, 2, 2584, 6, 1, 3}
+	want := []int{1, 2, 3, 5, 6, 2584}
+	reverseInsertSort(got)
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("insertSort():  got %v, want %v", got, want)
+	}
+}
+/*func TestMergeSort(t *testing.T) {
+	got := []int{5, 2, 4, 6, 1, 3}
+	want := []int{1, 2, 3, 4, 5, 6}
+	mergeSort(got)
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("insertSort():  got %v, want %v", got, want)
+	}
+}
+func TestMergeSortDuplicates(t *testing.T) {
+	got := []int{5, 2, 4, 6, 1, 3, 1}
+	want := []int{1, 1, 2, 3, 4, 5, 6}
+	mergeSort(got)
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("insertSort():  got %v, want %v", got, want)
+	}
+}
+
+func TestMergeSortLargeValues(t *testing.T) {
+	got := []int{5, 2, 2584, 6, 1, 3}
+	want := []int{1, 2, 3, 5, 6, 2584}
+	mergeSort(got)
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("insertSort():  got %v, want %v", got, want)
+	}
+}*/
