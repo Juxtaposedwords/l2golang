41a3c3c2f669ef8a34202c2d1bdc7f8d799562fc
juxtaposedwords <samuelmaloy@samuelmaloy-macbookpro.roam.corp.google.com>
2016-11-13 22:29:20 -0800

    gave up and checkin in robert's code. i'll develop out more unit tests

--- programming-pearls/src/p1c01/p1c01.go
+++ programming-pearls/src/p1c01/p1c01.go
@@ -1,1 +1,37 @@
 package p1c01
+
+import (
+	"log"
+)
+
+const maxNum = 100000
+
+type Bitmap []byte
+
+func NewBitmap() Bitmap {
+	b := make([]byte, maxNum/8, maxNum/8)
+	return Bitmap(b)
+}
+
+func (b Bitmap) SetBit(n int) {
+	if n < 0 || n > maxNum {
+		log.Fatalf("SetBit:  invalid argument: %d", n)
+	}
+	i, mask := getIndexAndMask(n)
+	b[i] = b[i] | mask
+}
+
+func (b Bitmap) CheckBit(n int) bool {
+	if n < 0 || n > maxNum {
+		log.Fatalf("CheckBit:  invalid argument: %d", n)
+	}
+	i, mask := getIndexAndMask(n)
+	return (b[i] & mask != 0)
+}
+
+func getIndexAndMask(n int) (int, byte) {
+	i := n / 8
+	bit := byte(n % 8)
+	mask := byte(1 << bit)
+	return i, mask	
+}
--- programming-pearls/src/p1c01/p1c01_test.go
+++ programming-pearls/src/p1c01/p1c01_test.go
@@ -4,6 +4,25 @@ import (
 	"testing"
 )
 
-func TestBitShift( t *testing.T){
-	
+func TestBitmap(t *testing.T) {
+
+	b := NewBitmap()
+
+	good := []int{0, 1, 1026, 1802, 29108}
+	bad := []int{2, 3, 16, 1024, 1025, 1027, 29107, 29109}
+	for _, v := range good {
+		b.SetBit(v)
+	}
+
+	for _, v := range good {
+		if got, want := b.CheckBit(v), true; got != want {
+			t.Errorf("b.CheckBit(%d): got %t, want %t", v, got, want)
+		}
+	}
+	for _, v := range bad {
+		if got, want := b.CheckBit(v), false; got != want {
+			t.Errorf("b.CheckBit(%d): got %t, want %t", v, got, want)
+		}
+	}
+
 }
