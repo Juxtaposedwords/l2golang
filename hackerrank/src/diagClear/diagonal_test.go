5741b9178c1cc9d3a2fff0e0e5a816354600e2f9
juxtaposedwords <samuelmaloy@samuelmaloy-macbookpro.roam.corp.google.com>
2016-11-26 15:38:29 -0800

    a bit cleaner, first steps on the experimental errors

--- hackerrank/src/diagClear/diagonal_test.go
+++ hackerrank/src/diagClear/diagonal_test.go
@@ -9,38 +9,35 @@ func TestDiagValue(t *testing.T) {
 	table := []struct {
 		input string
 		want  int
+		expError error
 	}{
 		{
 			input: `3
 11 2 4
 4 5 6
 10 8 -12`,
-			want: 15,
+			want: 15, 
+			expError: nil,
 		},
 		{
 			input: `2
 1 2
 1 4`,
 			want: 2,
+			expError: nil,
 		},
 	}
 
 	for _, test := range table {
 		r := strings.NewReader(test.input)
 		got, err := diagValue(r)
-		if err != nil {
-			// test messages should indicate what function failed, and what parameters
-			// it was passed, if it's possible, as it is in this case.
-			//
-			// failing that, the message should tell you which test case in the table it
-			// was using - either include the index in the message or include a description
-			// field in the test case definition.
+		switch {
+		case err != nil && test.expError != nil :
 			t.Errorf("diagValue(%q) returned %s", test.input, err)
-			// this continue is good.
-			continue
-		}
-		if got != test.want {
+		case got != test.want : 
 			t.Errorf("diag.Value(%q)\ngot %d\nwant %d", test.input, got, test.want)
+		default :
+			continue
 		}
 	}
 }
