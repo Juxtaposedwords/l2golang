fd75963f71dec3582aa1123b451df82d6f266987
juxtaposedwords <samuelmaloy@samuelmaloy-macbookpro.roam.corp.google.com>
2016-11-27 00:15:16 -0800

    checking in go code

--- hackerrank/src/diagClear/diagonal_test.go
+++ hackerrank/src/diagClear/diagonal_test.go
@@ -3,6 +3,7 @@ package diagClear
 import (
 	"strings"
 	"testing"
+	"errors"
 )
 
 func TestDiagValue(t *testing.T) {
@@ -26,14 +27,24 @@ func TestDiagValue(t *testing.T) {
 			want: 2,
 			expError: nil,
 		},
+		{
+			input: `2
+1 2
+1`,
+			want: 2,
+			expError: errors.New("EOF"),
+		},
+
 	}
 
 	for _, test := range table {
 		r := strings.NewReader(test.input)
 		got, err := diagValue(r)
 		switch {
+		case test.expError != err:
+			t.Errorf("Incorrect error. diagValue(%q) got %s\n want %s\n", test.input, err, test.expError )
 		case err != nil && test.expError != nil :
-			t.Errorf("diagValue(%q) returned %s", test.input, err)
+			t.Errorf("diagValue(%q) returned %z", test.input, err)
 		case got != test.want : 
 			t.Errorf("diag.Value(%q)\ngot %d\nwant %d", test.input, got, test.want)
 		default :
