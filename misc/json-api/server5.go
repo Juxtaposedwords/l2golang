0960fc64fe10095bb0f26796b5d876969fb3be73
Maloy <juxtaposedwords@gmail.com>
2017-02-14 07:02:18 +0000

    finally groking http

--- misc/json-api/server5.go
+++ misc/json-api/server5.go
@@ -2,56 +2,59 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"log"
 	"net/http"
 	"os"
 )
 
-type apiFunc func(r *http.Request) (*apiResponse, error)
-
-type apiResponse struct {
-	Name string `json:"name"`
+type spell struct {
+	Level       int    `json: "level"`
+	Name        string `json: "name"`
+	Description string `json: "description"`
 }
 
-func wrapApiFunc(f apiFunc) func(http.ResponseWriter, *http.Request) {
+type character struct {
+	Name  string `json: "name"`
+	Race  string `json: "race"`
+	Level int    `json: "level"`
+}
+type charComp func(r *http.Request) ([]byte, error)
+
+func magicHandler(f charComp) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ar, err := f(r)
+		b, err := f(r)
 		if err != nil {
-			log.Printf("error calling function: %s", err)
 			w.WriteHeader(http.StatusInternalServerError)
+			log.Fatal("There was an error loading : %s", err)
 			return
 		}
-		b, err := json.Marshal(*ar)
-		if err != nil {
-			log.Printf("error marshalling %v: %s", ar, err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
 		if _, err := w.Write(b); err != nil {
-			log.Printf("error writing response: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Fatal("There was an error writing: %s", err)
 		}
 	}
 }
 
-func apiFunc1(r *http.Request) (*apiResponse, error) {
-	return &apiResponse{"Calling func1"}, nil
+func charList(r *http.Request) ([]byte, error) {
+	t := []character{
+		{Level: 1, Name: "Maloy", Race: "Dwarf"},
+		{Level: 10, Name: "Claw", Race: "Mountain Lion"},
+		{Level: 19, Name: "Clem", Race: "Elf"},
+	}
+	return json.Marshal(t)
 }
 
-func apiFunc2(r *http.Request) (*apiResponse, error) {
-	return &apiResponse{"Calling func2"}, nil
+func spellList(r *http.Request) ([]byte, error) {
+	t := []spell{
+		{Level: 1, Name: "loud", Description: "Double the decibel, but no higher than 11."},
+		{Level: 2, Name: "frustrate", Description: "You speak for hours about the liberal agenda"},
+	}
+	return json.Marshal(t)
 }
-
-func apiFunc3(r *http.Request) (*apiResponse, error) {
-	return nil, errors.New("apiFunc3 always returns an error")
-}
-
 func main() {
-	http.HandleFunc("/api/func1", wrapApiFunc(apiFunc1))
-	http.HandleFunc("/api/func2", wrapApiFunc(apiFunc2))
-	http.HandleFunc("/api/func3", wrapApiFunc(apiFunc3))
-	port := os.Getenv("PORT")
-	log.Fatal(http.ListenAndServe(":"+port, nil))
 
+	http.HandleFunc("/api/spells", magicHandler(spellList))
+	http.HandleFunc("/api/character", magicHandler(charList))
+	PORT := os.Getenv("PORT")
+	log.Fatal(http.ListenAndServe(":"+PORT, nil))
 }
