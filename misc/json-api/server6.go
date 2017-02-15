3f58a4ef77965722e7cca7fac8a169e41de948dd
Maloy <juxtaposedwords@gmail.com>
2017-02-15 07:07:34 +0000

    post isn't working yet, but it's progress

--- misc/json-api/server6.go
+++ misc/json-api/server6.go
@@ -3,10 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type spell struct {
@@ -21,6 +23,25 @@ type character struct {
 	Level int    `json: "level"`
 }
 type charComp func(r *http.Request) ([]byte, error)
+
+// func magicJSON(title string) ([]interface{}, error) {
+// 	fn := "resources/" + title + ".json"
+// 	b, err := ioutil.ReadFile(fn)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	m := map[string]interface{}{
+// 		{"characters": character{},
+// 		{"spells": spell{}},
+// 	}
+// 	c := m[title]
+// 	o := []c{}
+// 	if err := json.Unmarshal(b, &o); err != nil {
+// 		return nil, err
+// 	}
+
+// 	return b, nil
+// }
 
 func magicHandler(f charComp) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -37,23 +58,84 @@ func magicHandler(f charComp) func(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func listChars(r *http.Request) ([]byte, error) {
-	t := []character{
-		{Level: 1, Name: "Maloy", Race: "Dwarf"},
-		{Level: 10, Name: "Claw", Race: "Mountain Lion"},
-		{Level: 19, Name: "Clem", Race: "Elf"},
+func loadJSON(title string) ([]byte, error) {
+	fn := "resources/" + title + ".json"
+	b, err := ioutil.ReadFile(fn)
+	if err != nil {
+		return nil, err
 	}
-	return json.Marshal(t)
+	return b, nil
+}
+
+func saveJSON(title string, b []byte) error {
+	fn := "resources/" + title + ".json"
+	err := ioutil.WriteFile(fn, b, 0600)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func loadSpells() ([]spell, error) {
+	b, err := loadJSON("spells")
+	if err != nil {
+		return nil, err
+	}
+	var s []spell
+	if err := json.Unmarshal(b, &s); err != nil {
+		return nil, err
+	}
+	return s, nil
+}
+
+func saveSpells(s []spell) error {
+	b, err := json.Marshal(s)
+	if err != nil {
+		return nil
+	}
+	err = saveJSON("spells", b)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func loadChars() ([]character, error) {
+	b, err := loadJSON("characters")
+	if err != nil {
+		return nil, err
+	}
+	var s []character
+	if err := json.Unmarshal(b, &s); err != nil {
+		return nil, err
+	}
+	return s, nil
+}
+
+func saveChars(c []character) error {
+	b, err := json.Marshal(c)
+	if err != nil {
+		return err
+	}
+	err = saveJSON("chracters", b)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func listChars(r *http.Request) ([]byte, error) {
+	return loadJSON("characters")
 }
 
 func getChar(r *http.Request) ([]byte, error) {
-	t := []character{
-		{Level: 1, Name: "Maloy", Race: "Dwarf"},
-		{Level: 10, Name: "Claw", Race: "Mountain Lion"},
-		{Level: 19, Name: "Clem", Race: "Elf"},
+	x, err := loadChars()
+	if err != nil {
+		return nil, err
 	}
 	char := r.URL.Path[len("/api/character/"):]
-	for _, e := range t {
+	for _, e := range x {
 		if e.Name == char {
 			return json.Marshal(e)
 		}
@@ -62,10 +144,9 @@ func getChar(r *http.Request) ([]byte, error) {
 
 }
 func listLevelChars(r *http.Request) ([]byte, error) {
-	t := []character{
-		{Level: 1, Name: "Maloy", Race: "Dwarf"},
-		{Level: 10, Name: "Claw", Race: "Mountain Lion"},
-		{Level: 19, Name: "Clem", Race: "Elf"},
+	t, err := loadChars()
+	if err != nil {
+		return nil, err
 	}
 	s, err := strconv.Atoi(r.URL.Path[len("/api/characters/"):])
 	if err != nil {
@@ -84,27 +165,20 @@ func listLevelChars(r *http.Request) ([]byte, error) {
 
 }
 func listSpells(r *http.Request) ([]byte, error) {
-	t := []spell{
-		{Level: 1, Name: "loud", Description: "Double the decibel, but no higher than 11."},
-		{Level: 1, Name: "distract", Description: "How do spell levels map to character levels again?"},
-		{Level: 2, Name: "frustrate", Description: "You speak for hours about the liberal agenda"},
-	}
-	return json.Marshal(t)
+	return loadJSON("spells")
 }
 
 func listLevelSpells(r *http.Request) ([]byte, error) {
-	t := []spell{
-		{Level: 1, Name: "loud", Description: "Double the decibel, but no higher than 11."},
-		{Level: 1, Name: "distract", Description: "How do spell levels map to character levels again?"},
-		{Level: 2, Name: "frustrate", Description: "You speak for hours about the liberal agenda"},
-	}
-	s, err := strconv.Atoi(r.URL.Path[len("/api/spells/"):])
+	t, err := loadSpells()
 	if err != nil {
 		return nil, err
 	}
 
+	s, err := strconv.Atoi(r.URL.Path[len("/api/spells/"):])
+	if err != nil {
+		return nil, err
+	}
 	var o []spell
-
 	for _, e := range t {
 		if e.Level == s {
 			o = append(o, e)
@@ -117,10 +191,9 @@ func listLevelSpells(r *http.Request) ([]byte, error) {
 }
 
 func getSpell(r *http.Request) ([]byte, error) {
-	t := []spell{
-		{Level: 1, Name: "loud", Description: "Double the decibel, but no higher than 11."},
-		{Level: 1, Name: "distract", Description: "How do spell levels map to character levels again?"},
-		{Level: 2, Name: "frustrate", Description: "You speak for hours about the liberal agenda"},
+	t, err := loadSpells()
+	if err != nil {
+		return nil, err
 	}
 	s := r.URL.Path[len("/api/spell/"):]
 	for _, e := range t {
@@ -129,7 +202,34 @@ func getSpell(r *http.Request) ([]byte, error) {
 		}
 	}
 	return []byte(fmt.Sprintf("There is no such spell as %s!", s)), nil
+}
 
+func addSpell(r *http.Request) ([]byte, error) {
+	_, err := loadSpells()
+	if err != nil {
+		return nil, err
+	}
+	if r.Method != "POST" {
+		return nil, fmt.Errorf("Attempted to GET, not PUT")
+	}
+	r.ParseForm()
+	fmt.Printf("%v", r.Form["level"])
+
+	_, err = strconv.Atoi(strings.TrimSpace(strings.Join(r.Form["level"], "")))
+	if err != nil {
+		return nil, err
+	}
+	// n := spell{
+	// 	Level:       l,
+	// 	Name:        r.Form["name"][1],
+	// 	Description: r.Form["description"][1],
+	// }
+	// t = append(t, n)
+	// err = saveSpells(t)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	return nil, nil
 }
 
 func main() {
@@ -137,8 +237,9 @@ func main() {
 	http.HandleFunc("/api/spells", magicHandler(listSpells))
 	http.HandleFunc("/api/spells/", magicHandler(listLevelSpells))
 	http.HandleFunc("/api/spell/", magicHandler(getSpell))
+	http.HandleFunc("/api/spell/add", magicHandler(addSpell))
 	http.HandleFunc("/api/characters", magicHandler(listChars))
-	http.HandleFunc("/api/characters/", magicHandler(listLevelChars))
+	http.HandleFunc("/api/characters/", magicHandler(listChars))
 	http.HandleFunc("/api/character/", magicHandler(getChar))
 	PORT := os.Getenv("PORT")
 	log.Fatal(http.ListenAndServe(":"+PORT, nil))
