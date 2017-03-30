6a189c3a5c1211102615e8aebd301519a5732e4c
Maloy <juxtaposedwords@gmail.com>
2017-03-30 16:41:34 +0000

    working on the code

--- cracking/01-strings/04-URLify/src/URLify/URLify.go
+++ cracking/01-strings/04-URLify/src/URLify/URLify.go
@@ -4,10 +4,34 @@ package URLify
 // has sufficient space at the end to hold the additional characters, and that you are given the "true"
 //  length of the string. (Note: If implementing in Java, please use a character array so that you can
 // perform this operation in place.)
-func URLify(s string, l int) string {
-	r := []rune(s)
-	for i < len(r) {
+import (
+	"fmt"
+	"strings"
+)
 
+func URLify(s string, l int) (string, error) {
+	if len(s) < l {
+		return "", fmt.Errorf("Length must be longer than the length of the string")
 	}
-	return ""
+	last := len(s) - 1
+	r := []rune(s)
+	beforeString, afterString := strings.Count(string(r[0:l]), " "), strings.Count(string(r[l:]), " ")
+	if afterString != beforeString*2 {
+		return "", fmt.Errorf("Impossible to pad, not enough trailing whitepsace")
+	}
+
+	for i := l - 1; i > 0; i-- {
+		switch {
+		case string(r[i]) == " " && last != l-1:
+			r[last-2], r[last-1], r[last] = rune('%'), rune('2'), rune('0')
+			last -= 3
+		case string(r[i]) != " ":
+			r[last] = r[i]
+			last--
+		case string(r[i]) == " " && last == l-1:
+			continue
+		}
+		fmt.Printf("i: %d l: %s, String is now: %s\n  last: %d\n", i, string(r[i]), string(r), last)
+	}
+	return string(r), nil
 }
--- cracking/01-strings/04-URLify/src/URLify/URLify_test.go
+++ cracking/01-strings/04-URLify/src/URLify/URLify_test.go
@@ -10,14 +10,17 @@ func TestURLify(t *testing.T) {
 		haveLength int
 		want       string
 	}{
-		{"Mr John Smith      ", 13, "Mr%20John%20Smith"},
-		{"Muad'Dib   The  Desert Mouse       ", 35, "Muad'Dib%20%20%20The%20%20Desert%20Mouse"},
+		{"Mr John Smith    ", 13, "Mr%20John%20Smith"},
+		{"Muad'Dib   The  Desert Mouse            ", 28, "Muad'Dib%20%20%20The%20%20Desert%20Mouse"},
 		{"  way To The Golden Path            ", 24, "%20%20way%20To%20The%20Golden%20Path"},
+		{"PaulAtreides", 12, "PaulAtreides"},
 	}
 
 	for _, e := range tt {
-		if e.want != URLify(e.haveString, e.haveLength) {
-			t.Errorf("There was a problem with %+v\n Got: %s, want: %s ", e, URLify(e.haveString, e.haveLength), e.want)
+		got, err := URLify(e.haveString, e.haveLength)
+		want := e.want
+		if got != want {
+			t.Errorf("URLify(%q, %d): got %q, want %q error: %s", e.haveString, e.haveLength, got, want, err)
 		}
 	}
 }
