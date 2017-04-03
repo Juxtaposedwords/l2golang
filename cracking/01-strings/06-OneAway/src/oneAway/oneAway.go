716ff9f15edf1425eb2af93742c3bfc74e675a03
Maloy <juxtaposedwords@gmail.com>
2017-04-03 03:57:12 +0000

    Fence posts error fixed

--- cracking/01-strings/06-OneAway/src/oneAway/oneAway.go
+++ cracking/01-strings/06-OneAway/src/oneAway/oneAway.go
@@ -1,8 +1,5 @@
 package oneAway
 
-// One Away: There are three types of edits that can be performed on strings: insert a character,
-// remove a character, or replace a character. Given two strings, write a function to check if they are
-// one edit (or zero edits) away.
 import (
 	"fmt"
 )
@@ -17,36 +14,29 @@ func OneAway(a, b string) bool {
 	}
 	j, k, diff := 0, 0, 0
 	fmt.Printf("\n")
-	for j < maxLen && k < maxLen {
-		fmt.Printf("c: %s d: %s  j: %d k: %d  ", string(c[j]), string(d[k]), j, k)
+	for j < maxLen-1 && k < maxLen-1 {
 		switch {
-		case len(c) <= j+1:
+		// check if the lengths are different
+		case len(c) == j+1 && len(d)-1 > k:
 			k++
-			fmt.Printf("first\n")
-		case len(d) <= k+1:
+		case len(d) == k+1 && len(c)-1 > j:
 			j++
-			fmt.Printf("second\n")
 		// see if they are the same
 		case c[j] == d[k]:
 			j++
 			k++
-			fmt.Printf("third\n")
 			continue
 		// check to see if one was inserted or deleted in the second
 		case len(d) >= k+1 && c[j] == d[k+1]:
-			fmt.Printf("fourth\n")
 			k++
 		// check to see if one was inserted or deleted in the first
 		case len(c) >= j+1 && c[j+1] == d[k]:
-			fmt.Printf("fifth\n")
 			j++
 		default:
-			fmt.Printf("default\n")
 			j++
 			k++
 		}
 		diff++
-		fmt.Printf(" diff: %d\n", diff)
 		if diff >= 2 {
 			return false
 		}
