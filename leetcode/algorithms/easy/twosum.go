f267c86e3157b8c9f7d013375be2601a18a519eb
Maloy <juxtaposedwords@gmail.com>
2018-08-26 07:23:50 +0000

    added some comments

--- leetcode/algorithms/easy/twosum.go
+++ leetcode/algorithms/easy/twosum.go
@@ -1,15 +1,22 @@
 package TwoSum
 
+import "fmt"
+
 // Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 // Assume:
 //   1. only one possible match
 //   2. elements may not be used twice
 
+// My solution:
+//    Steps through the arraywith a sub array, meaning we have N^2.
 func twoSum(nums []int, target int) []int {
+	fmt.Println("new")
+
 	for i, _ := range nums {
-		for j, _ := range nums[i:] {
-			if nums[i]+nums[j] == target {
-				return []int{i, j}
+		for j, _ := range nums[i+1:] {
+			fmt.Printf("i: %d, j: %d\n", i, j)
+			if nums[i]+nums[j+i+1] == target {
+				return []int{i, j + i + 1}
 			}
 		}
 	}
--- leetcode/algorithms/easy/twosum_test.go
+++ leetcode/algorithms/easy/twosum_test.go
@@ -12,8 +12,9 @@ func TestTwoSum(t *testing.T) {
 		want   []int
 	}{
 		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
-		{[]int{2, 7, 11, 15}, 17, []int{0, 3}},
+		{[]int{2, 7, 11, 15}, 26, []int{2, 3}},
 		{[]int{2, 7, 11, 15}, 0, []int{}},
+		{[]int{3, 2, 4}, 6, []int{1, 2}},
 	}
 
 	for _, v := range have {
