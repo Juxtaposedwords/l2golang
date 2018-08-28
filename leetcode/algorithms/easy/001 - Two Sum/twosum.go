2d6d3a5e774441be767c744c25cae7525264ab6b
Maloy <juxtaposedwords@gmail.com>
2018-08-28 00:52:09 +0000

    forgot to check in intial exercise

---
+++ leetcode/algorithms/easy/001 - Two Sum/twosum.go
@@ -0,0 +1,25 @@
+package TwoSum
+
+import "fmt"
+
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+// Assume:
+//   1. only one possible match
+//   2. elements may not be used twice
+
+// My solution:
+//    Steps through the arraywith a sub array, meaning we have N! runtime.
+//       We'll be stepping over n-i 
+func twoSum(nums []int, target int) []int {
+	fmt.Println("new")
+
+	for i, _ := range nums {
+		for j, _ := range nums[i+1:] {
+			fmt.Printf("i: %d, j: %d\n", i, j)
+			if nums[i]+nums[j+i+1] == target {
+				return []int{i, j + i + 1}
+			}
+		}
+	}
+	return []int{}
+}
