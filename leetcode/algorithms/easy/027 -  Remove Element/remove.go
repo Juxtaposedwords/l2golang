6c8eabf315dbb1ad543905127dc7bea31aec73dc
Maloy <juxtaposedwords@gmail.com>
2018-09-03 05:06:19 +0000

    Added comments

--- leetcode/algorithms/easy/027 -  Remove Element/remove.go
+++ leetcode/algorithms/easy/027 -  Remove Element/remove.go
@@ -1,5 +1,20 @@
 package remove
 
+/*
+
+Prompt:
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+Explanation:
+This is a classic 'swap' example.
+
+Go through each character. If it isn't the offending character, 'val', swap it
+with the last known good point
+
+
+*/
 func removeElement(nums []int, val int) int {
 	next := 0
 	for i := 0; i < len(nums); i++ {
