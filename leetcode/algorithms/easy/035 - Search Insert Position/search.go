def09954aabb1c52fa167ecb9704e5999b387de0
Maloy <juxtaposedwords@gmail.com>
2018-09-08 04:48:37 +0000

    Added comments after understanding this attempt focuses on the ends rather than the median (Which makes for more a tractable results due to the exit clause being out ends being the same

--- leetcode/algorithms/easy/035 - Search Insert Position/search.go
+++ leetcode/algorithms/easy/035 - Search Insert Position/search.go
@@ -10,14 +10,18 @@ In order to get the most performant time we can use the binary search to ensure
 */
 func searchInsert(nums []int, target int) int {
 
-	// Now we also store that as the location, as we'll be halving that while
-	//    we search
+	// Store the bounds of what we're search the first and last
 	start, end := 0, len(nums)-1
+
+	// the exit case is when our window collapses on itself
 	for start <= end {
+		//find the middle of our search parameter
 		m := (start + end) / 2
 		if nums[m] < target {
+			//now close the window up
 			start = m + 1
 		} else {
+			// or close the window down
 			end = m - 1
 		}
 	}
