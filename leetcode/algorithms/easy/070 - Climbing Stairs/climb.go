213e20b2fa961a1bc35afc5dee2063835a6b1f28
Maloy <juxtaposedwords@gmail.com>
2018-09-17 04:32:52 +0000

    and now with iterative solutions

--- leetcode/algorithms/easy/070 - Climbing Stairs/climb.go
+++ leetcode/algorithms/easy/070 - Climbing Stairs/climb.go
@@ -14,6 +14,10 @@ So this is fibonacci and the hint is seen in the difference of steps, as is the
 var seen = map[int]int{0: 0, 1: 1, 2: 2, 3: 3}
 
 func climbStairs(n int) int {
+	return iterStairs(n)
+}
+
+func recurClimbStairs(n int) int {
 	_, ok := seen[n]
 	if ok {
 		return seen[n]
@@ -21,4 +25,17 @@ func climbStairs(n int) int {
 
 	seen[n] = climbStairs(n-2) + climbStairs(n-1)
 	return seen[n]
+}
+
+func iterStairs(n int) int {
+	if n == 1 {
+		return 1
+	}
+	first, second := 1, 2
+	for i := 3; i <= n; i++ {
+		third := first + second
+		first = second
+		second = third
+	}
+	return second
 }
