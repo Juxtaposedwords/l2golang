8f576cf3d2229dfbab13744a489dc7272711d6c7
Maloy <juxtaposedwords@gmail.com>
2019-12-26 21:08:15 +0000

    adding fuel tests since they now work

--- advent/2019/01/fuel.go
+++ advent/2019/01/fuel.go
@@ -1,8 +1,12 @@
 package fuel
 
 func required(input uint64) uint64 {
+	if input < 7 {
+		return 0
+	}
 	return uint64(input/3) - 2
 }
+// TotalRequired computes the amount of fuel required given a list of weights.
 func TotalRequired(inputs []uint64) uint64 {
 	var sum uint64
 	for _, entry := range inputs {
--- advent/2019/01/fuel_test.go
+++ advent/2019/01/fuel_test.go
@@ -15,11 +15,6 @@ func TestRequired(t *testing.T) {
 			have: 14,
 			want: 2,
 		},
-		{
-			desc: "big Endian",
-			have: 1,
-			want: 0,
-		},
 		{desc: "provide example #1",
 			have: 12,
 			want: 2,
@@ -37,7 +32,7 @@ func TestRequired(t *testing.T) {
 		{
 			desc: "upper limit of input",
 			have: 18446744073709551615,
-			want: 0,
+			want: 6148914691236517203,
 		},
 	}
 	for _, tc := range tests {
@@ -47,6 +42,39 @@ func TestRequired(t *testing.T) {
 			got := required(tc.have)
 			if got != tc.want {
 				t.Errorf("Required(%d) returned %d. Expected: %d", tc.have, got, tc.want)
+			}
+		})
+	}
+}
+func TestTotalRequired(t *testing.T) {
+	tests := []struct {
+		desc string
+		have []uint64
+		want uint64
+	}{
+		{
+			desc: "happy path",
+			have: []uint64{7},
+			want: 1,
+		},
+		{
+			desc: "0 valued",
+			have: []uint64{0,0,0,1,2,3,4,5,6},
+			want: 0,
+		},
+		{
+			desc: "Largest possible value",
+			have: []uint64{18446744073709551615,18446744073709551615,18446744073709551615},
+			want: 18446744073709551609,
+		},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.desc, func(t *testing.T) {
+			t.Parallel()
+			got := TotalRequired(tc.have)
+			if got != tc.want {
+				t.Errorf("TotalRequired(%d) returned %d . Expected: %d", tc.have, got, tc.want)
 			}
 		})
 	}
