d83b5227b839df98b949a8f77c5447e1ea6e28c2
Maloy <juxtaposedwords@gmail.com>
2019-12-27 23:40:39 +0000

    adding git ignore to not add the generated binaries

--- advent/2019/02/src/internal/intCode/intCode_test.go
+++ advent/2019/02/src/internal/intCode/intCode_test.go
@@ -30,6 +30,26 @@ func TestRequired(t *testing.T) {
 			want: []int{1,1,2,3,99},
 		},
 		{
+			desc: "Provided example: 1",
+			have: []int{1,0,0,0,99},
+			want: []int{2,0,0,0,99},
+		},
+		{
+			desc: "Provided example: 2",
+			have: []int{2,3,0,3,99},
+			want: []int{2,3,0,6,99},
+		},
+		{
+			desc: "Provided example: 3",
+			have: []int{2,4,4,5,99,0},
+			want: []int{2,4,4,5,99,9801},
+		},
+		{
+			desc: "Provided example: 4",
+			have: []int{1,1,1,4,99,5,6,0,99},
+			want: []int{30,1,1,4,2,5,6,0,99},
+		},
+		{
 			desc: "invalid opcode",
 			have: []int{4,1},
 			wantError: status.Error(codes.InvalidArgument, "bad opcode"),
