d3067b64c4b046fe0eefc15b992c073373fa4d3d
Maloy <juxtaposedwords@gmail.com>
2019-12-27 05:40:30 +0000

    have readInts passing functions now. So checking in

--- advent/2019/01/fuel.go
+++ advent/2019/01/fuel.go
@@ -1,16 +1,58 @@
 package fuel
 
+import (
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+//	"github.com/google/logger"
+	"io"
+	"strconv"
+	"bufio"
+	"os"
+)
+
+const maxUINT64 = ^uint64(0)
 func required(input uint64) uint64 {
 	if input < 7 {
 		return 0
 	}
 	return uint64(input/3) - 2
 }
-// TotalRequired computes the amount of fuel required given a list of weights.
-func TotalRequired(inputs []uint64) uint64 {
+
+func totalRequired(inputs []uint64) uint64 {
 	var sum uint64
 	for _, entry := range inputs {
 		sum += required(entry)
 	}
 	return sum
+}
+
+// ReadFuel takes in the path to a file which contains all the weights you have.
+//  The amount of fuel necessary for each item is computed and added to a running 
+//  sum which is returned at the end. 
+func ReadFuel(filepath string) (uint64, error) {
+	f, err := os.Open(filepath)
+	if err != nil {
+		return 0, status.Error(codes.FailedPrecondition, err.Error())
+	}
+	return readInts(bufio.NewReader(f))
+
+}
+func readInts(r io.Reader) (uint64, error) {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+	var result uint64
+	for scanner.Scan() {
+		x, err := strconv.ParseUint(scanner.Text(), 10,64)
+		if err != nil {
+			return result, status.Error(codes.InvalidArgument, err.Error())
+		}
+		diff := required(x)
+	//	logger.Infof("%d %d",maxUINT64-result,x)
+		if maxUINT64  - result < diff {
+			return 0, status.Error(codes.Internal, "integer overflow")
+		}
+
+		result +=diff 
+	}
+	return result, scanner.Err()
 }
--- advent/2019/01/fuel_test.go
+++ advent/2019/01/fuel_test.go
@@ -2,6 +2,11 @@ package fuel
 
 import (
 	"testing"
+	"fmt"
+	"io"
+	"strings"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func TestRequired(t *testing.T) {
@@ -24,13 +29,18 @@ func TestRequired(t *testing.T) {
 			have: 6,
 			want: 0,
 		},
-		{
+		{ 
 			desc: "free fuel lower limit",
 			have: 0,
 			want: 0,
 		},
 		{
 			desc: "upper limit of input",
+			have: 18446744073709551615,
+			want: 6148914691236517203,
+		},
+		{
+			desc: "input too large input",
 			have: 18446744073709551615,
 			want: 6148914691236517203,
 		},
@@ -46,35 +56,50 @@ func TestRequired(t *testing.T) {
 		})
 	}
 }
-func TestTotalRequired(t *testing.T) {
+func TestReadInts(t *testing.T) {
 	tests := []struct {
 		desc string
-		have []uint64
-		want uint64
+		have io.Reader
+		wantResp uint64
+		wantError error 
 	}{
 		{
 			desc: "happy path",
-			have: []uint64{7},
-			want: 1,
+			have: strings.NewReader("9"),
+			wantResp: 1,
 		},
 		{
 			desc: "0 valued",
-			have: []uint64{0,0,0,1,2,3,4,5,6},
-			want: 0,
+			have:  strings.NewReader("0 0 0 1 2 3 4 5 6 7 8"),
+						wantResp: 0,
 		},
 		{
 			desc: "Largest possible value",
-			have: []uint64{18446744073709551615,18446744073709551615,18446744073709551615},
-			want: 18446744073709551609,
+			have: strings.NewReader(fmt.Sprintf("%d",uint64(^uint64(0)/3)-2)),
+			wantResp:  uint64(^uint64(0)/3)-2,
+		},
+		{
+			desc: "uint overflow",
+			have: strings.NewReader("18446744073709551615 18446744073709551615 18446744073709551615 18446744073709551615 18446744073709551615 18446744073709551615 18446744073709551615 18446744073709551615"),
+			wantError: status.Error(codes.Internal, "uint64 overflow"),
+		},
+		{
+			desc: "uint too big",
+			have: strings.NewReader("1844674407370955161518446744073709551615184467440737095516151844674407370955161518446744073709551615184467440737095516151844674407370955161518446744073709551615"),
+			wantError: status.Error(codes.InvalidArgument, "uint64 too large"),
 		},
 	}
 	for _, tc := range tests {
 		tc := tc
 		t.Run(tc.desc, func(t *testing.T) {
 			t.Parallel()
-			got := TotalRequired(tc.have)
-			if got != tc.want {
-				t.Errorf("TotalRequired(%d) returned %d . Expected: %d", tc.have, got, tc.want)
+			got, err := readInts(tc.have)
+			if got, want := status.Code(err), status.Code(tc.wantError); got != want {
+				t.Errorf("readints() unexpected error. want: %s got: %s",got,want )
+				return
+			}
+			if got != tc.wantResp {
+				t.Errorf("TotalRequired() returned %d . Expected: %d",  got, tc.wantResp)
 			}
 		})
 	}
