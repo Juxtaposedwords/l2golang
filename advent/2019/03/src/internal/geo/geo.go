04b3757c69dd5299b1ba5bdee361c0ab82937903
Maloy <juxtaposedwords@gmail.com>
2019-12-28 04:27:05 +0000

    checking in the passing code for solution 3

---
+++ advent/2019/03/src/internal/geo/geo.go
@@ -0,0 +1,115 @@
+package geo
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type location struct {
+	x int
+	y int
+}
+type cardinality int 
+const (
+	up cardinality = iota +1
+	down
+	left 
+	right 
+)
+
+var (
+	directionRE  = regexp.MustCompile(`^[UDRL]\d+$`)
+)
+
+// ClosestIntersection gives the closest intersection of the two list of coordinates. 
+func ClosestIntersection( left, right []string) (int, error) {
+	leftLocations, err := getLocations(left)
+	if err != nil {
+		return 0, err
+	}
+	rightLocations,err := getLocations(right)
+	if err != nil {
+		return 0, err
+	}
+	leftVisits := map[string]bool{}
+	for _, loc := range leftLocations {
+		leftVisits[fmt.Sprintf("%03d%03d",loc.x,loc.y)] = true
+	}
+	
+	var lowestDistance int
+	for _, loc := range rightLocations {
+		if !leftVisits[fmt.Sprintf("%03d%03d",loc.x,loc.y)]{
+			continue
+		}  
+		if  lowestDistance ==  0 || taxiDistance(loc) < lowestDistance{
+				lowestDistance = taxiDistance(loc)
+			}
+		} 
+	
+	return lowestDistance, nil
+}
+
+func taxiDistance(loc *location) int {
+	return absVal(loc.x) + absVal(loc.y)
+}
+
+func absVal(input int ) int {
+	if input < 0 {
+		return input * -1
+	}
+	return input
+}
+
+func getLocations( directions []string ) ([]*location, error) {
+	if len(directions) == 0 {
+		return nil, status.Error(codes.FailedPrecondition, "empty slice provided")
+	}
+	locs := map[string]*location{}
+	x, y := 0,0
+	for _, instruction:=range  directions{
+		dir, dist, err := parse(instruction)
+		if err != nil {
+			return nil,err
+		}
+		for i:= 0; i <dist; i++ {
+			switch dir {
+			case up:
+				x++
+			case down:
+				x--
+			case left:
+				y--
+			case right:
+				y++
+			}
+			locs[fmt.Sprintf("%03d%03d",x,y)] = &location{x: x, y:y}
+		}
+	}
+	var output []*location
+	for _, value := range locs {
+		output = append(output,value)
+	}
+	return output, nil
+}
+
+func parse(input string) (cardinality,int, error) {
+	if ! directionRE.MatchString(input){
+		return 0,0, status.Error(codes.InvalidArgument, "input does not match a valid direction")
+	}
+	characters := strings.Split(input, "")
+	distance, _ :=strconv.Atoi(strings.Join(characters[1:],""))
+	switch characters[0] {
+	case "U":
+		return up,distance, nil
+	case "D":
+		return down,distance, nil
+	case "L":
+		return left,distance, nil
+	case "R":
+		return right,distance, nil
+	}
+	return 0,0, nil
+}
