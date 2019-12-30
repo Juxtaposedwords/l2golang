c1b7df14fb860b33cd34a8eaeaae35b79ee911d9
Maloy <juxtaposedwords@gmail.com>
2019-12-30 04:43:10 +0000

    checking in code since it all works now

--- advent/2019/04/src/internal/combo/combo.go
+++ advent/2019/04/src/internal/combo/combo.go
@@ -1,48 +1,117 @@
 package combo
 
 import (
+	"github.com/google/logger"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-)
-/*Brute uses brute force approach to find all the possible combinations of possibilie between the two numbers in which all options in which no digit is descending and there are at least two repeated digits.
-*/
-func Brute(lower, upper int) (int,error) {
-	lowerBounds, upperBounds := sliceify(lower), sliceify(upper)
 
+	"sync"
+	"sync/atomic"
+)
+
+/*Brute uses brute force approach to find all the possible combinations of possibilie between the two numbers in which all options in which no digit is descending and there are at least two repeated digits.
+ */
+func Brute(lower, upper int) (int, error) {
 	switch {
-	case len(lowerBounds) != len(upperBounds):
-		return 0, status.Errorf(codes.InvalidArgument, "different digit lengths: lower %d upper %d ",lower, upper)
+	case digits(lower) != digits(upper):
+		return 0, status.Errorf(codes.InvalidArgument, "different digit lengths: lower %d upper %d ", lower, upper)
 	case lower > upper:
-		return 0, status.Errorf(codes.InvalidArgument, "lower greater than upper: lower %d upper %d ",lower, upper)
+		return 0, status.Errorf(codes.InvalidArgument, "lower greater than upper: lower %d upper %d ", lower, upper)
 	}
-	var result int
-	for i := lower; i <= upper; i ++ {
-		if valid(i) {
-			result++
-		}
+	var result int32
+	var wg sync.WaitGroup
+	for i := lower; i <= upper; i++ {
+
+		wg.Add(1)
+		go func(digit int) {
+			if valid(digit) {
+				atomic.AddInt32(&result, 1)
+			}
+			wg.Done()
+		}(i)
 	}
-return result, nil
+	wg.Wait()
+	return int(result), nil
 }
 
- func digits(i int) (count int) {
- 	for i != 0 {
- 		i /= 10
- 		count = count + 1
- 	}
- 	return count
- }
+/*Permutations uses a recursive programming solution to find all possible solutions between the two provided bounds.
+ */
+func Permutations(lower, upper int) (int, error) {
+	switch {
+	case digits(lower) != digits(upper):
+		return 0, status.Errorf(codes.InvalidArgument, "different digit lengths: lower %d upper %d ", lower, upper)
+	case lower > upper:
+		return 0, status.Errorf(codes.InvalidArgument, "lower greater than upper: lower %d upper %d ", lower, upper)
+	case digits(lower) == 1:
+		return 0, nil
+	}
+	
+	var combinations int
+	for i := lower+1; i < upper; i++ {
+		logger.Infof("i: %d\n", i)
+		var repeat bool
+		i, repeat = valid2(intToSlice(i))
+		if repeat {
+			combinations++
+		}
+	}
+	return combinations, nil
+}
+
+func valid2(digits []int) (int, bool) {
+	lowest := digits[0]
+	var repeat bool
+	for j := 1; j < len(digits); j++ {
+		switch {
+		case digits[j] < lowest:
+			for k := j - 1; k < len(digits); k++ {
+				digits[k] = lowest
+			}
+			return sliceToInt(digits) - 1, false
+		case digits[j] == lowest:
+			repeat = true
+		default:
+			lowest = digits[j]
+		}
+	}
+	return sliceToInt(digits), repeat
+}
+func setter(index int, input []int) int {
+	for i := index; index < len(input); i++ {
+		input[i] = input[index]
+	}
+	return sliceToInt(input)
+
+}
+func digits(i int) (count int) {
+	for i != 0 {
+		i /= 10
+		count = count + 1
+	}
+	return count
+}
+
+func sliceToInt(s []int) int {
+	res := 0
+	op := 1
+	for i := len(s) - 1; i >= 0; i-- {
+		res += s[i] * op
+		op *= 10
+	}
+	return res
+}
 func valid(input int) bool {
 	var intSlice []int
-	intSlice = sliceify(input)
+	intSlice = intToSlice(input)
 	if len(intSlice) == 1 {
 		return false
 	}
 	var repeat bool
 	for i := 1; i < len(intSlice); i++ {
 		switch {
-		case intSlice[i-1] > intSlice[i]: 	
+		case intSlice[i-1] > intSlice[i]:
 			return false
-		case intSlice[i-1] == intSlice[i] :
+		case intSlice[i-1] == intSlice[i]:
 			repeat = true
 		}
 	}
@@ -52,15 +121,15 @@ func valid(input int) bool {
 	return true
 
 }
-func recurSliceify(input int, output []int) []int {
-    if input != 0 {
-        i := input % 10
-        output = append([]int{i}, output...)
-        return recurSliceify(input/10, output)
-    }
-    return output
+func recurIntToSlice(input int, output []int) []int {
+	if input != 0 {
+		i := input % 10
+		output = append([]int{i}, output...)
+		return recurIntToSlice(input/10, output)
+	}
+	return output
 }
-func sliceify( input int) []int {
+func intToSlice(input int) []int {
 	var output []int
-	return recurSliceify(input, output)
+	return recurIntToSlice(input, output)
 }
--- advent/2019/04/src/internal/combo/combo_test.go
+++ advent/2019/04/src/internal/combo/combo_test.go
@@ -8,37 +8,43 @@ import (
 
 func TestBrute(t *testing.T) {
 	tests := []struct {
-		desc      string
-		haveLower int
-		haveUppper  int
-		want      int
-		wantError error
+		desc       string
+		haveLower  int
+		haveUppper int
+		want       int
+		wantError  error
 	}{
-				{
-		desc: "happyPath",
-		haveLower: 11,
-		haveUppper: 12,
-		want: 1,
-		},
-						{
-		desc: "lower and upper equal",
-		haveLower: 22,
-		haveUppper: 22,
-		want: 1,
+		{
+			desc:       "happyPath",
+			haveLower:  11,
+			haveUppper: 12,
+			want:       1,
 		},
 		{
-		desc: "lower > upper",
-		haveLower: 12,
-		haveUppper: 11,
-		wantError: status.Error(codes.InvalidArgument, "lower > upper"),
-		},
-		{
-		desc: "upper and lower length mismatch",
-		haveLower: 120,
-		haveUppper: 11,
-		wantError: status.Error(codes.InvalidArgument, "length mismatch"),
+			desc:       "large input",
+			haveLower:  122,
+			haveUppper: 986,
+			want:       71,
 		},
 
+		{
+			desc:       "lower and upper equal",
+			haveLower:  22,
+			haveUppper: 22,
+			want:       1,
+		},
+		{
+			desc:       "lower > upper",
+			haveLower:  12,
+			haveUppper: 11,
+			wantError:  status.Error(codes.InvalidArgument, "lower > upper"),
+		},
+		{
+			desc:       "upper and lower length mismatch",
+			haveLower:  120,
+			haveUppper: 11,
+			wantError:  status.Error(codes.InvalidArgument, "length mismatch"),
+		},
 	}
 	for _, tc := range tests {
 		tc := tc
@@ -51,6 +57,72 @@ func TestBrute(t *testing.T) {
 			}
 			if got != tc.want {
 				t.Errorf("Brute() mismatch want: %d got: %d", tc.want, got)
+			}
+		})
+	}
+}
+func TestPermutations(t *testing.T) {
+	tests := []struct {
+		desc       string
+		haveLower  int
+		haveUppper int
+		want       int
+		wantError  error
+	}{
+		{
+			desc:       "happyPath",
+			haveLower:  11,
+			haveUppper: 12,
+			want:       0,
+		},
+		{
+			desc:       "two digits",
+			haveLower:  11,
+			haveUppper: 99,
+			want:       7,
+		},
+		{
+			desc:       "small input",
+			haveLower:  122,
+			haveUppper: 986,
+			want:       70,
+		},
+		{
+			desc:       "large input",
+			haveLower:  125730,
+			haveUppper: 579381,
+			want:       2081,
+		},
+		{
+			desc:       "lower and upper equal",
+			haveLower:  22,
+			haveUppper: 22,
+			want:       0,
+		},
+		{
+			desc:       "lower > upper",
+			haveLower:  12,
+			haveUppper: 11,
+			wantError:  status.Error(codes.InvalidArgument, "lower > upper"),
+		},
+		{
+			desc:       "upper and lower length mismatch",
+			haveLower:  120,
+			haveUppper: 11,
+			wantError:  status.Error(codes.InvalidArgument, "length mismatch"),
+		},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.desc, func(t *testing.T) {
+			t.Parallel()
+			got, err := Permutations(tc.haveLower, tc.haveUppper)
+			if got, want := status.Code(err), status.Code(tc.wantError); got != want {
+				t.Errorf("Permutations() unexpected error. want: %s got: %s", want, got)
+				return
+			}
+			if got != tc.want {
+				t.Errorf("Permutations() mismatch want: %d got: %d", tc.want, got)
 			}
 		})
 	}
