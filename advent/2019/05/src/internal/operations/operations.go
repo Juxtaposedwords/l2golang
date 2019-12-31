2ce8b8f5ebbc54f9e477878ff3e834b68dcaa4b0
Maloy <juxtaposedwords@gmail.com>
2019-12-31 04:47:39 +0000

    adding a package for extracting operation codes and parameter modes

---
+++ advent/2019/05/src/internal/operations/operations.go
@@ -0,0 +1,87 @@
+package operations
+
+import (
+	"fmt"
+	"strings"
+//	"github.com/google/logger"
+// 	"io/ioutil"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// Code tells us the operations which can be taken.
+type Code int
+
+/*	Add (3 parameters) - param c = param a + param b
+    Multiply (3 parameters) - param c = param a * param b
+    Copy(1 parameters) - write the value of param b to the item in index of param b
+ 	Print(1 parameter) - print/"output" the parameter given
+	Terminate - required ending Code */
+const (
+	Add Code = iota + 1
+	Multiply
+	Copy
+	Print
+	Terminate Code = 99
+)
+
+var codeMap = map[string]Code{
+	"01": Add,
+	"02": Multiply,
+	"03": Copy,
+	"04": Print,
+	"99": Terminate,
+}
+
+// Mode represents the two ways to write.
+type Mode int
+
+// Position - use  the associated parameter value as index
+// Immediate - use the associated paramter value as a value
+const (
+	Position Mode = iota + 1
+	Immediate
+)
+
+var modeMap = map[string]Mode{
+	"0": Position,
+	"1": Immediate,
+}
+
+//InstructionSet contains all the mappings of paramters input to modes.
+type InstructionSet struct {
+	Operation Code
+	First     Mode
+	Second    Mode
+	Third     Mode
+}
+
+// Parse turns an operation Code into an output of an instruction set.
+func Parse(input int) (*InstructionSet, error) {
+//	defer logger.Init("LoggerExample", true, false, ioutil.Discard)
+
+	paddedInput := fmt.Sprintf("%05d", input)
+	if len(paddedInput) > 5 {
+		return nil, status.Error(codes.FailedPrecondition, "invalid integer length")
+	}
+
+	digits := strings.Split(paddedInput, "")
+	opCode, ok := codeMap[fmt.Sprintf("%s%s", digits[3], digits[4])]
+	if !ok {
+		return nil, status.Error(codes.InvalidArgument, "invalid operation Code provided")
+	}
+
+	first, firstOK := modeMap[digits[2]]
+	second, secondOK := modeMap[digits[1]]
+	third, thirdOK := modeMap[digits[0]]
+	if !firstOK || !secondOK || !thirdOK {
+		return nil, status.Error(codes.InvalidArgument, "invalid operation Mode provided")
+	}
+
+	return &InstructionSet{
+		Operation: opCode,
+		First:     first,
+		Second:    second,
+		Third:     third,
+	}, nil
+}
---
+++ advent/2019/05/src/internal/operations/operations_test.go
@@ -0,0 +1,82 @@
+package operations
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	tests := []struct {
+		desc          string
+		have          int
+		wantResp      *InstructionSet
+		wantErrorCode codes.Code
+	}{
+		{
+			desc: "create add insturctions",
+			have: 1,
+			wantResp: &InstructionSet{
+				Operation: Add,
+				First:     Position,
+				Second:    Position,
+				Third:     Position,
+			},
+		},
+		{
+			desc: "create multiple instructions all with immediate",
+			have: 11101,
+			wantResp: &InstructionSet{
+				Operation: Add,
+				First:     Immediate,
+				Second:    Immediate,
+				Third:     Immediate,
+			},
+		},
+
+		{
+			desc: "int too long",
+			have: 8675309,
+			wantErrorCode: codes.FailedPrecondition,
+		},
+		{
+			desc: "invalid operation code",
+			have: 87,
+			wantErrorCode: codes.InvalidArgument,
+		},
+		{
+			desc: "invalid first mode",
+			have: 201,
+			wantErrorCode: codes.InvalidArgument,
+		},
+		{
+			desc: "invalid second mode",
+			have: 2001,
+			wantErrorCode: codes.InvalidArgument,
+		},
+		{
+			desc: "invalid third mode",
+			have: 20001,
+			wantErrorCode: codes.InvalidArgument,
+		},
+
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.desc, func(t *testing.T) {
+			t.Parallel()
+			gotResp, err := Parse(tc.have)
+			if got, want := status.Code(err), tc.wantErrorCode; got != want {
+				t.Errorf("Parse() unexpected error. want: %s got: %s", want, got)
+				return
+			}
+			if tc.wantErrorCode != codes.OK {
+				return
+			}
+			if diff := cmp.Diff(gotResp, tc.wantResp); diff != "" {
+				t.Errorf("Parse() mismatch (-want +got):\n%s\n got: %#v want: %#v", diff, gotResp, tc.wantResp)
+			}
+		})
+	}
+}
